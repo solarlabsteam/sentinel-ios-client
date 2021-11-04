ecd17be6943ef4bbaacb1fab999524b781457aa5
Lika <lika@solarlabs.ee>
2021-11-04 12:42:23 +0200

    Make target builable

---
+++ DVPNApp/SentinelDVPNmacOS/SentinelDVPNmacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SentinelDVPNmacOSApp.swift
+//  SentinelDVPNmacOS
+//
+//  Created by Lika Vorobeva on 04.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct SentinelDVPNmacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
