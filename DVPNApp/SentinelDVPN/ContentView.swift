d433bd4e7058ebaca37815c42a2530367cace850
VictoriaKostyleva <kostyleva.viktoriya@gmail.com>
2022-01-21 15:56:53 +0200

    Show AccountInfo as popover

--- DVPNApp/SentinelDVPN/ContentView.swift
+++ DVPNApp/SentinelDVPN/ContentView.swift
@@ -8,17 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var showAccountPopover = false
+    
     var body: some View {
         NavigationView {
             ModulesFactory.shared.makeNodeSelectionModule()
         }
         .toolbar {
             ToolbarItem(placement: .primaryAction) {
-                Button(action: {}) {
+                Button(action: { self.showAccountPopover.toggle() }) {
                     Asset.Navigation.account.image.asImage
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                 }
+                .popover(isPresented: self.$showAccountPopover, arrowEdge: .bottom) {
+                    ModulesFactory.shared.makeAccountInfoScene()
+                }
+                .buttonStyle(PlainButtonStyle())
                 .frame(width: 25, height: 25)
             }
         }
