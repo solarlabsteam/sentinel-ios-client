709563409c67bf985d8937ba207ef584f006bbf8
VictoriaKostyleva <kostyleva.viktoriya@gmail.com>
2022-01-21 15:38:13 +0200

    Add Sharing view

---
+++ DVPNApp/SentinelDVPN/Scenes/AccountInfo/Subviews/SharingPicker.swift
@@ -0,0 +1,46 @@
+//
+//  SharingPicker.swift
+//  SentinelDVPN
+//
+//  Created by Victoria Kostyleva on 21.01.2022.
+//
+
+import SwiftUI
+
+struct SharingPicker: NSViewRepresentable {
+    @Binding var isPresented: Bool
+    var sharingItems: [Any] = []
+
+    func makeNSView(context: Context) -> NSView {
+        let view = NSView()
+        return view
+    }
+
+    func updateNSView(_ nsView: NSView, context: Context) {
+        if isPresented {
+            let picker = NSSharingServicePicker(items: sharingItems)
+            picker.delegate = context.coordinator
+            
+            DispatchQueue.main.async {
+                picker.show(relativeTo: .zero, of: nsView, preferredEdge: .minY)
+            }
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(owner: self)
+    }
+
+    class Coordinator: NSObject, NSSharingServicePickerDelegate {
+        let owner: SharingPicker
+
+        init(owner: SharingPicker) {
+            self.owner = owner
+        }
+
+        func sharingServicePicker(_ sharingServicePicker: NSSharingServicePicker, didChoose service: NSSharingService?) {
+            sharingServicePicker.delegate = nil
+            self.owner.isPresented = false
+        }
+    }
+}
